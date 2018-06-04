2f6703eb8f50d8a9de8b93296483177fd5a62ee3
Anton <as.kudryav@gmail.com>
2018-06-04 16:25:25 +0300

    Фикс миграции

--- migrations/m180601_103915_change_mailbox_table.php
+++ migrations/m180601_103915_change_mailbox_table.php
@@ -10,6 +10,7 @@ class m180601_103915_change_mailbox_table extends Migration
     // Use up()/down() to run migration code without a transaction.
     public function up()
     {
+        $this->alterColumn('mailbox', 'host', $this->string());
         $this->renameColumn('mailbox', 'host', 'buyer');
         $this->alterColumn('mailbox', 'port', 'varchar(20)');
         $this->renameColumn('mailbox', 'port', 'phone');
@@ -19,8 +20,7 @@ class m180601_103915_change_mailbox_table extends Migration
 
     public function down()
     {
-        echo "m180601_103915_change_mailbox_table cannot be reverted.\n";
-
-        return false;
+        $this->renameColumn('mailbox', 'buyer', 'host');
+        $this->renameColumn('mailbox', 'phone', 'port');
     }
 }
