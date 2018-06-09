69c1260146abfd6887f9e0db1ee5f8143fb38c09
Anton <as.kudryav@gmail.com>
2018-06-09 11:22:58 +0300

    поправка в миграции

--- migrations/m180609_071054_change_server_data.php
+++ migrations/m180609_071054_change_server_data.php
@@ -12,7 +12,7 @@ class m180609_071054_change_server_data extends Migration
      */
     public function safeUp()
     {
-        $this->update('server', ['host'=>'gmail.com'], ['host'=>'Gmail']);
+        $this->update('server', ['host'=>'gmail.com'], ['host'=>'Google']);
         $this->update('server', ['host'=>'yahoo.com'], ['host'=>'Yahoo']);
         $this->update('server', ['host'=>'outlook.com'], ['host'=>'Outlook']);
         $this->update('server', ['host'=>'aol.com'], ['host'=>'AOL']);
